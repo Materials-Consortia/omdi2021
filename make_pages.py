115f5494d8154de72da60f1a3f64d9036ea340f2
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-04-10 02:01:19 +0200

    First edition
    Make a similar page as to the regular OPTIMADE site.
    Make the navigation bar small and sticky as one scrolls down.
    
    Use Jinja2 and python-markdown to create the HTML files, mimick the
    workflow from providers-dashboard.

---
+++ make_pages.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Generate HTML page for the OPTIMADE ontology workshop."""
+from pathlib import Path
+import shutil
+
+from jinja2 import Environment, PackageLoader, select_autoescape
+import markdown
+
+# Out-folders
+OUT_FOLDER = Path("build").resolve()
+ASSETS_FOLDER = "assets"
+
+# Absolute paths
+pwd = Path(__file__).parent.resolve()
+ASSETS_FOLDER_ABS = pwd / f"_{ASSETS_FOLDER}"
+
+# In-folders
+IN_FOLDER = Path("ontology_workshop").resolve()
+
+
+def make_pages():
+    """Create the rendered page."""
+    # Create output folder, copy static assets files
+    if OUT_FOLDER.exists():
+        shutil.rmtree(OUT_FOLDER)
+    OUT_FOLDER.mkdir()
+    shutil.copytree(ASSETS_FOLDER_ABS, OUT_FOLDER / ASSETS_FOLDER)
+
+    env = Environment(
+        loader=PackageLoader("ontology_workshop"),
+        autoescape=select_autoescape(["html", "xml"]),
+    )
+
+    all_data = {}
+
+    # Get content
+    with open(IN_FOLDER / "content.md") as handle:
+        all_data["content"] = markdown.markdown(handle.read())
+
+    # Write main overview index
+    rendered = env.get_template("default.html.j2").render(**all_data)
+    outfile = OUT_FOLDER / "index.html"
+    with open(outfile, "w") as handle:
+        handle.write(rendered)
+
+
+if __name__ == "__main__":
+    make_pages()
---
+++ ontology_workshop/content.md
@@ -0,0 +1,116 @@
+# Lorem ipsum
+
+This is a test section.
+Isn't it great?
+
+Some more text for tests :)
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
+
+Text far away !
